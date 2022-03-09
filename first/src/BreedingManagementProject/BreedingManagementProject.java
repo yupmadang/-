818575a1b1145772f458faff9a05341dd982ee8d
Yongjin Jeon <yjin1185@gmail.com>
2022-03-09 15:34:13 +0900

    2주차 BreedingManagementProject 메뉴입니다. ppt와 강의에 print를 사용해서 출력하라고 되어 있어 println을 사용합니다.

---
+++ first/src/BreedingManagementProject/BreedingManagementProject.java
@@ -0,0 +1,13 @@
+package BreedingManagementProject;
+
+public class BreedingManagementProject {
+
+	public static void main(String[] args) {
+		System.out.println("1, Add Insect");
+		System.out.println("2, Delete Insect");
+		System.out.println("3, Edit Insect");
+		System.out.println("4, View Insect");
+		System.out.println("5, Exit Insect");
+	}
+
+}
