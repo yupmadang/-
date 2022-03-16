5b855cc10dbaf90676ac685c3bbc0ffd306b09ea
Yongjin Jeon <yjin1185@gmail.com>
2022-03-17 00:07:07 +0900

    2019011965 전용진 3주차 프로젝트

--- first/src/BreedingManagementProject/BreedingManagementProject.java
+++ first/src/BreedingManagementProject/BreedingManagementProject.java
@@ -4,9 +4,34 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BreedingManagementProject {
-
+	static Scanner stdin = new Scanner(System.in);
+	
+	public static void add_Insect(){
+		System.out.println("개체명을 입력 : "); String ins = stdin.next();
+		System.out.println("추가된 개체 : " + ins);
+	}
+	
+	public static void Delete_Insect(){
+		System.out.println("삭제할 개체 입력 : "); String ins = stdin.next();
+		System.out.println("삭제된 개체 : " + ins);
+	}
+	
+	public static void Edit_Insect() {
+		System.out.println("수정할 개체 입력 : "); String ins = stdin.next();
+		System.out.println("수정된 개체 : " + ins);
+	}
+	
+	public static void View_Insect() {
+		System.out.println("출력할 개체 입력 : "); String ins = stdin.next();
+		System.out.println("출력된 개체들 : " + ins);
+	}
+	
+	public static void View_Stock() {
+		System.out.println("출력할 물품 입력 : "); String ins = stdin.next();
+		System.out.println("출력한 뭏품 : " + ins);
+	}
+	
 	public static void main(String[] args) {
-		Scanner stdin = new Scanner(System.in);
 		int menu;
 		do {
 			try {
@@ -17,22 +42,27 @@ public class BreedingManagementProject {
 				
 				case 1:
 					System.out.println("1, Add Insect");
+					add_Insect();
 					System.out.println();
 					break;
 				case 2:
 					System.out.println("2, Delete Insect");
+					Delete_Insect();
 					System.out.println();
 					break;
 				case 3:
 					System.out.println("3, Edit Insect");
+					Edit_Insect();
 					System.out.println();
 					break;
 				case 4:
 					System.out.println("4, View Insect");
+					View_Insect();
 					System.out.println();
 					break;
 				case 5:
 					System.out.println("5, View Stock");
+					View_Stock();
 					System.out.println();
 					break;
 				}
@@ -51,12 +81,7 @@ public class BreedingManagementProject {
 			}
 		}while(true);
 		
-		
-		/*System.out.println("1, Add Insect"); 프로젝트 2주차 변경사항 비교용
-		System.out.println("2, Delete Insect");
-		System.out.println("3, Edit Insect");
-		System.out.println("4, View Insect");
-		System.out.println("5, Exit Insect");*/
+		stdin.close();
 	}
 
 }
