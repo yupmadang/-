121c997e3ffb6ffc0328dfb9905defce2ed8ea26
Yongjin Jeon <yjin1185@gmail.com>
2022-03-25 03:11:01 +0900

    4주차 management project입니다.

--- first/src/BreedingManagementProject/BreedingManagementProject.java
+++ first/src/BreedingManagementProject/BreedingManagementProject.java
@@ -1,76 +1,83 @@
 package BreedingManagementProject;
 
 import java.util.InputMismatchException;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class BreedingManagementProject {
-	static Scanner stdin = new Scanner(System.in);
 	
-	public static void add_Insect(){
-		System.out.println("개체명을 입력 : "); String ins = stdin.next();
-		System.out.println("추가된 개체 : " + ins);
+	public static void add_Insect(HashSet <String> n, String name){
+		n.add(name);
+		if(n.contains(name) == false) {
+			System.out.println("개체의 추가에 실패하였습니다.");
+		}
 	}
 	
-	public static void Delete_Insect(){
-		System.out.println("삭제할 개체 입력 : "); String ins = stdin.next();
-		System.out.println("삭제된 개체 : " + ins);
+	public static void Delete_Insect(HashSet <String> n, String index){
+		for(int i = 0; i < n.size(); i++) {
+			if(n.contains(index)==true) {
+				n.remove(index);
+				if(n.contains(index)==true) {
+					System.out.println("삭제에 실패하였습니다.");
+				}
+			}
+		}
 	}
 	
-	public static void Edit_Insect() {
-		System.out.println("수정할 개체 입력 : "); String ins = stdin.next();
-		System.out.println("수정된 개체 : " + ins);
+	public static void Edit_Insect(HashSet <String> n, String index, String index2) {
+		n.remove(index);
+		n.add(index2);
+		if(n.contains(index2) == false) {
+			System.out.println("값의 변경에 실패하였습니다.");
+		}
 	}
 	
-	public static void View_Insect() {
-		System.out.println("출력할 개체 입력 : "); String ins = stdin.next();
-		System.out.println("출력된 개체들 : " + ins);
-	}
-	
-	public static void View_Stock() {
-		System.out.println("출력할 물품 입력 : "); String ins = stdin.next();
-		System.out.println("출력한 뭏품 : " + ins);
+	public static void View_Insect(HashSet <String> n) {
+			System.out.println(n.toString());
 	}
 	
 	public static void main(String[] args) {
+		HashSet <String> ins = new HashSet <String> ();
+		Scanner stdin = new Scanner(System.in);
 		int menu;
 		do {
 			try {
 				System.out.println("Breeding Management Project입니다");
-				System.out.println("(1)개체목록 추가 (2)개체목록 제거 (3)개체목록 편집 (4)개체목록 출력 (5)용품제고 확인 (6)종료");
+				System.out.println("(1)개체목록 추가 (2)개체목록 제거 (3)개체목록 편집 (4)개체목록 출력 (5)종료");
 				System.out.print("메뉴를 입력해 주십시오 : "); menu = stdin.nextInt();
 				switch(menu) {
 				
 				case 1:
 					System.out.println("1, Add Insect");
-					add_Insect();
+					String insect = stdin.next();
+					add_Insect(ins, insect);
 					System.out.println();
 					break;
 				case 2:
 					System.out.println("2, Delete Insect");
-					Delete_Insect();
+					String insect2 = stdin.next();
+					Delete_Insect(ins, insect2);
 					System.out.println();
 					break;
 				case 3:
 					System.out.println("3, Edit Insect");
-					Edit_Insect();
+					String insect3 = stdin.next();
+					String insect4 = stdin.next();
+					Edit_Insect(ins,insect3,insect4);
 					System.out.println();
 					break;
 				case 4:
 					System.out.println("4, View Insect");
-					View_Insect();
-					System.out.println();
-					break;
-				case 5:
-					System.out.println("5, View Stock");
-					View_Stock();
+					View_Insect(ins);
 					System.out.println();
 					break;
 				}
-				if(menu > 6 || menu < 1) {
+			
+				if(menu > 5 || menu < 1) {
 					System.out.println("지원하지 않는 모드입니다.");
 				}
 				
-				if(menu == 6) {
+				if(menu == 5) {
 					System.out.println("프로젝트를 종료합니다.");
 					System.out.println();
 					break;
@@ -83,5 +90,5 @@ public class BreedingManagementProject {
 		
 		stdin.close();
 	}
-
 }
+
