f8a5cf51c30bb47455bb6ec3c3b9ec2108a7881a
Yongjin Jeon <yjin1185@gmail.com>
2022-05-02 20:29:07 +0900

    오류로 인해 재설정한 프로젝트입니다. 감사합니다

---
+++ Breeding/src/breedingManagement/Insect.java
@@ -0,0 +1,44 @@
+package breedingManagement;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Insect {
+	//현재 시간을 불러옴
+	SimpleDateFormat format1 = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");		
+	Date time = new Date();
+	String time1 = format1.format(time);
+	//클래스의 변수를 선언, private로 접근제어자 선언으로 직접적인 변수로의 접근은 같은 클래스 내에서만 가능
+	protected int id;
+	protected String name;
+	private double weight;
+	//사용자 정의 생성자 생성
+	public Insect(int id, String name, double weight) {
+		this.id = id;
+		this.name = name;
+		this.weight = weight;
+	}
+	public Insect() {}//edit 인스턴스를 위해 따로 디폴크 생성자를 선언함
+	//get,set메서드를 사용하여 외부에서의 변수에 직접적인 접근 차단, edit인스턴스에서 활용
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String toString() {
+		return ("[개체 번호 :"+id+", 개체명 : "+name+", 무게 : "+weight+"g ,투입일 : "+time1+"]");
+	}
+}
---
+++ Breeding/src/breedingManagement/Stock.java
@@ -0,0 +1,37 @@
+package breedingManagement;
+
+public class Stock extends Insect { //Insect 클래스 상속
+
+	private String name2;
+	private int num = 1;
+
+	public Stock(int id, String name, String name2, int num) { //생성자 생성
+		super(id, name, 0.0); //부모 클래스의 생성자를 참조
+		this.name2 = name2;
+		this.num = num;
+	}
+
+	public Stock() {//edit2를 위한 디폴트 생성자
+	}
+
+	public void setNum(int num) {
+		this.num= num;	
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public String getName2() {
+		return name2;
+	}
+
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+
+	public String toString() {//출력할 toString 메서드 재정의
+		return "[개체 번호 : "+ id + ", 개체명 : " + name +", "+"톱밥 종류 : "+name2+", 교체 횟수: "+num+"]";
+	}
+
+}
