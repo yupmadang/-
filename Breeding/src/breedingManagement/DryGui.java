cc373cf16a61c685c5598a12a891b1436514f107
Yongjin Jeon <yjin1185@gmail.com>
2022-06-09 10:11:09 +0900

    최종 프로젝트 결과입니다

--- Breeding/src/breedingManagement/DryGui.java
+++ Breeding/src/breedingManagement/DryGui.java
@@ -35,16 +35,22 @@ public class DryGui implements ActionListener{
 	//파일에서 값을 불러오고 추가하기 위한 선언
 	BreedingMode breedingMode = new BreedingMode();
 	LogClass logger = new LogClass("log.txt");
+	private JTextField TFDId;
+	private JTextField TFDLen;
+	private JTextField TFDLabel;
+	private JTextField TFEId;
+	private JTextField TFELabel;
+	private JTextField TFEState;
 	//gui생성자
 	public DryGui() {
 		initialize();
 	}
 	//gui를 구성하는 initialize메서드
 	private void initialize() {
-		frame = new JFrame();
+		frame = new  JFrame();
 		frame.setType(Type.UTILITY);
 		frame.setBounds(100, 100, 450, 300);
-		frame.setSize(800, 400);
+		frame.setSize(1000, 500);
 		frame.setResizable(false);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
@@ -129,9 +135,9 @@ public class DryGui implements ActionListener{
 		Delete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try{
-					String id = TFId.getText();
-					String label = TFLabel.getText();
-					String len = TFLen.getText();
+					String id = TFDId.getText();
+					String label = TFDLabel.getText();
+					String len = TFDLen.getText();
 					//3가지 표본 정보와 일치할 경우 파일에서 그 객체를 제거
 					for(DryInsect i : logger.getDIList()) {
 						if((""+i.getId()).equals(id) && i.getLabel().equals(label) && i.getLength() == Double.parseDouble(len)) {
@@ -142,11 +148,9 @@ public class DryGui implements ActionListener{
 								//객체가 제거된 후 테이블 갱신
 								for(int j = 0;;j++) {
 									model.removeRow(j);
-									TFId.setText("");
-									TFName.setText("");
-									TFLen.setText("");
-									TFState.setText("");
-									TFLabel.setText("");
+									TFDId.setText("");
+									TFDLen.setText("");
+									TFDLabel.setText("");
 									table.updateUI();
 								}
 								
@@ -169,9 +173,9 @@ public class DryGui implements ActionListener{
 				//파일에 저장된 객체를 순회하여 일치하는 값이 있을 경우 state로 상채를 갱신함
 				try{
 					for(int i = 0; i < logger.getDIList().size(); i++) {
-						int id = Integer.parseInt(TFId.getText()); 
-						String label = TFLabel.getText(); 
-						String state = TFState.getText(); 
+						int id = Integer.parseInt(TFEId.getText()); 
+						String label = TFELabel.getText(); 
+						String state = TFEState.getText(); 
 					
 						if(logger.getDIList().get(i).getId() == id && logger.getDIList().get(i).getLabel().equals(label)) {
 							breedingMode.Edit_Insect(id, label, state);
@@ -182,11 +186,9 @@ public class DryGui implements ActionListener{
 				}catch (NumberFormatException e2) {
 					return;
 				}
-					TFId.setText("");
-					TFName.setText("");
-					TFLen.setText("");
-					TFState.setText("");
-					TFLabel.setText("");
+					TFEId.setText("");
+					TFEState.setText("");
+					TFELabel.setText("");
 					
 					table.updateUI();
 				}
@@ -248,24 +250,17 @@ public class DryGui implements ActionListener{
 		splitPane.setLeftComponent(panel_1);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
 		gbl_panel_1.columnWidths = new int[]{0, 50, 0};
-		gbl_panel_1.rowHeights = new int[]{0, 15, 0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel_1.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel_1.setLayout(gbl_panel_1);
-		
-		JLabel lblNewLabel_6 = new JLabel("개체추가");
-		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
-		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNewLabel_6.gridx = 1;
-		gbc_lblNewLabel_6.gridy = 0;
-		panel_1.add(lblNewLabel_6, gbc_lblNewLabel_6);
 		
 		JLabel lblNewLabel_1 = new JLabel("표본번호");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.NORTHEAST;
 		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 1;
+		gbc_lblNewLabel_1.gridy = 0;
 		panel_1.add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
 		TFId = new JTextField();
@@ -273,7 +268,7 @@ public class DryGui implements ActionListener{
 		gbc_TFId.insets = new Insets(0, 0, 5, 0);
 		gbc_TFId.fill = GridBagConstraints.HORIZONTAL;
 		gbc_TFId.gridx = 1;
-		gbc_TFId.gridy = 1;
+		gbc_TFId.gridy = 0;
 		panel_1.add(TFId, gbc_TFId);
 		TFId.setColumns(10);
 		
@@ -282,7 +277,7 @@ public class DryGui implements ActionListener{
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_2.gridx = 0;
-		gbc_lblNewLabel_2.gridy = 2;
+		gbc_lblNewLabel_2.gridy = 1;
 		panel_1.add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
 		TFName = new JTextField();
@@ -290,7 +285,7 @@ public class DryGui implements ActionListener{
 		gbc_TFName.insets = new Insets(0, 0, 5, 0);
 		gbc_TFName.fill = GridBagConstraints.HORIZONTAL;
 		gbc_TFName.gridx = 1;
-		gbc_TFName.gridy = 2;
+		gbc_TFName.gridy = 1;
 		panel_1.add(TFName, gbc_TFName);
 		TFName.setColumns(10);
 		
@@ -299,7 +294,7 @@ public class DryGui implements ActionListener{
 		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_3.gridx = 0;
-		gbc_lblNewLabel_3.gridy = 3;
+		gbc_lblNewLabel_3.gridy = 2;
 		panel_1.add(lblNewLabel_3, gbc_lblNewLabel_3);
 		
 		TFLen = new JTextField();
@@ -307,7 +302,7 @@ public class DryGui implements ActionListener{
 		gbc_TFLen.insets = new Insets(0, 0, 5, 0);
 		gbc_TFLen.fill = GridBagConstraints.HORIZONTAL;
 		gbc_TFLen.gridx = 1;
-		gbc_TFLen.gridy = 3;
+		gbc_TFLen.gridy = 2;
 		panel_1.add(TFLen, gbc_TFLen);
 		TFLen.setColumns(10);
 		
@@ -316,7 +311,7 @@ public class DryGui implements ActionListener{
 		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_4.gridx = 0;
-		gbc_lblNewLabel_4.gridy = 4;
+		gbc_lblNewLabel_4.gridy = 3;
 		panel_1.add(lblNewLabel_4, gbc_lblNewLabel_4);
 		
 		TFState = new JTextField();
@@ -324,25 +319,141 @@ public class DryGui implements ActionListener{
 		gbc_TFState.insets = new Insets(0, 0, 5, 0);
 		gbc_TFState.fill = GridBagConstraints.HORIZONTAL;
 		gbc_TFState.gridx = 1;
-		gbc_TFState.gridy = 4;
+		gbc_TFState.gridy = 3;
 		panel_1.add(TFState, gbc_TFState);
 		TFState.setColumns(10);
 		
 		JLabel lblNewLabel_5 = new JLabel("라벨");
 		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
-		gbc_lblNewLabel_5.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_5.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_5.gridx = 0;
-		gbc_lblNewLabel_5.gridy = 5;
+		gbc_lblNewLabel_5.gridy = 4;
 		panel_1.add(lblNewLabel_5, gbc_lblNewLabel_5);
 		
 		TFLabel = new JTextField();
 		GridBagConstraints gbc_TFLabel = new GridBagConstraints();
+		gbc_TFLabel.insets = new Insets(0, 0, 5, 0);
 		gbc_TFLabel.fill = GridBagConstraints.HORIZONTAL;
 		gbc_TFLabel.gridx = 1;
-		gbc_TFLabel.gridy = 5;
+		gbc_TFLabel.gridy = 4;
 		panel_1.add(TFLabel, gbc_TFLabel);
 		TFLabel.setColumns(10);
+		
+		JLabel lblNewLabel_7 = new JLabel("표본제거");
+		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_7.gridx = 1;
+		gbc_lblNewLabel_7.gridy = 5;
+		panel_1.add(lblNewLabel_7, gbc_lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("표본번호");
+		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+		gbc_lblNewLabel_8.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_8.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_8.gridx = 0;
+		gbc_lblNewLabel_8.gridy = 6;
+		panel_1.add(lblNewLabel_8, gbc_lblNewLabel_8);
+		
+		TFDId = new JTextField();
+		GridBagConstraints gbc_TFDId = new GridBagConstraints();
+		gbc_TFDId.insets = new Insets(0, 0, 5, 0);
+		gbc_TFDId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_TFDId.gridx = 1;
+		gbc_TFDId.gridy = 6;
+		panel_1.add(TFDId, gbc_TFDId);
+		TFDId.setColumns(10);
+		
+		JLabel lblNewLabel_9 = new JLabel("길이");
+		GridBagConstraints gbc_lblNewLabel_9 = new GridBagConstraints();
+		gbc_lblNewLabel_9.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_9.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_9.gridx = 0;
+		gbc_lblNewLabel_9.gridy = 7;
+		panel_1.add(lblNewLabel_9, gbc_lblNewLabel_9);
+		
+		TFDLen = new JTextField();
+		GridBagConstraints gbc_TFDLen = new GridBagConstraints();
+		gbc_TFDLen.insets = new Insets(0, 0, 5, 0);
+		gbc_TFDLen.fill = GridBagConstraints.HORIZONTAL;
+		gbc_TFDLen.gridx = 1;
+		gbc_TFDLen.gridy = 7;
+		panel_1.add(TFDLen, gbc_TFDLen);
+		TFDLen.setColumns(10);
+		
+		JLabel lblNewLabel_10 = new JLabel("라벨");
+		GridBagConstraints gbc_lblNewLabel_10 = new GridBagConstraints();
+		gbc_lblNewLabel_10.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_10.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_10.gridx = 0;
+		gbc_lblNewLabel_10.gridy = 8;
+		panel_1.add(lblNewLabel_10, gbc_lblNewLabel_10);
+		
+		TFDLabel = new JTextField();
+		GridBagConstraints gbc_TFDLabel = new GridBagConstraints();
+		gbc_TFDLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_TFDLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_TFDLabel.gridx = 1;
+		gbc_TFDLabel.gridy = 8;
+		panel_1.add(TFDLabel, gbc_TFDLabel);
+		TFDLabel.setColumns(10);
+		
+		JLabel lblNewLabel_11 = new JLabel("표본편집");
+		GridBagConstraints gbc_lblNewLabel_11 = new GridBagConstraints();
+		gbc_lblNewLabel_11.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_11.gridx = 1;
+		gbc_lblNewLabel_11.gridy = 9;
+		panel_1.add(lblNewLabel_11, gbc_lblNewLabel_11);
+		
+		JLabel lblNewLabel_12 = new JLabel("표본번호");
+		GridBagConstraints gbc_lblNewLabel_12 = new GridBagConstraints();
+		gbc_lblNewLabel_12.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_12.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_12.gridx = 0;
+		gbc_lblNewLabel_12.gridy = 10;
+		panel_1.add(lblNewLabel_12, gbc_lblNewLabel_12);
+		
+		TFEId = new JTextField();
+		GridBagConstraints gbc_TFEId = new GridBagConstraints();
+		gbc_TFEId.insets = new Insets(0, 0, 5, 0);
+		gbc_TFEId.fill = GridBagConstraints.HORIZONTAL;
+		gbc_TFEId.gridx = 1;
+		gbc_TFEId.gridy = 10;
+		panel_1.add(TFEId, gbc_TFEId);
+		TFEId.setColumns(10);
+		
+		JLabel lblNewLabel_13 = new JLabel("라벨");
+		GridBagConstraints gbc_lblNewLabel_13 = new GridBagConstraints();
+		gbc_lblNewLabel_13.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_13.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_13.gridx = 0;
+		gbc_lblNewLabel_13.gridy = 11;
+		panel_1.add(lblNewLabel_13, gbc_lblNewLabel_13);
+		
+		TFELabel = new JTextField();
+		GridBagConstraints gbc_TFELabel = new GridBagConstraints();
+		gbc_TFELabel.insets = new Insets(0, 0, 5, 0);
+		gbc_TFELabel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_TFELabel.gridx = 1;
+		gbc_TFELabel.gridy = 11;
+		panel_1.add(TFELabel, gbc_TFELabel);
+		TFELabel.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("\uBCC0\uACBD\uC0C1\uD0DC");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_6.gridx = 0;
+		gbc_lblNewLabel_6.gridy = 12;
+		panel_1.add(lblNewLabel_6, gbc_lblNewLabel_6);
+		
+		TFEState = new JTextField();
+		GridBagConstraints gbc_TFEState = new GridBagConstraints();
+		gbc_TFEState.fill = GridBagConstraints.HORIZONTAL;
+		gbc_TFEState.gridx = 1;
+		gbc_TFEState.gridy = 12;
+		panel_1.add(TFEState, gbc_TFEState);
+		TFEState.setColumns(10);
 		frame.setVisible(true);
 	}
 
